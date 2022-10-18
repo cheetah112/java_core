fa34d88c03ee0200a99b3f65be0d6d517f68fc0e
cheetah112 <nonpvp123@gmail.com>
2022-10-18 13:44:58 +0800

    Create WrongPass.java

---
+++ Java/Test2/Test2/src/View/WrongPass.java
@@ -0,0 +1,10 @@
+package View;
+
+public class WrongPass {
+    public void wrongPass() {
+        System.out.println("1.Dang nhap lai.\n2.Quen Mat Khau.");
+    }
+    public void fogotPass() {
+        System.out.println("Nhap Email: ");
+    }
+}
