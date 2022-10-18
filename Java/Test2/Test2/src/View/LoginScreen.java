bce26dcc4c72a76e645fd2b46a13e379adef91e9
cheetah112 <nonpvp123@gmail.com>
2022-10-18 13:44:53 +0800

    Update LoginScreen.java

--- Java/Test2/Test2/src/View/LoginScreen.java
+++ Java/Test2/Test2/src/View/LoginScreen.java
@@ -2,6 +2,6 @@ package View;
 
 public class LoginScreen {
     public void loginScreen() {
-        System.out.println("Chao mung , ban co the thuc hien cac cong viec sau:\n1.Thay doi username\n2.Thay doi email\n3.Thay doi mat khau\n4.Dang xuat\0.Thoat chuong trinh");
+        System.out.println("1.Thay doi username\n2.Thay doi email\n3.Thay doi mat khau\n4.Dang xuat\n0.Thoat chuong trinh");
     }
 }
