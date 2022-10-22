a123004b44de6697b62518f6b8c0305f57fd73bc
cheetah112 <nonpvp123@gmail.com>
2022-10-22 18:22:40 +0800

    Update App.java

--- Java/Test2/Test2/src/Controller/App.java
+++ Java/Test2/Test2/src/Controller/App.java
@@ -24,35 +24,37 @@ public class App {
                 }else{
                     if(userModel.checkUsername(users, user)){
                         System.out.println("UserName da duoc su dung.");
-                        }else{
-                            if(userModel.checkPass(user)){
-                            users.add(user);
-                            System.out.println(users.toString());  
+                    }else{
+                        if(userModel.checkPass(user)){
+                        users.add(user);
+                        //In Users de check code.
+                        //System.out.println(users.toString());  
                             }
+                        }
                 }
             }
-        }
             
             //option - Dang nhap.
             else if (i == 1){    
             User u = userModel.login(users);
             if(u == null){
 
-            }
-            else{
-                int j = sc.nextInt();
-            if(j==1){
-                userModel.changeUsername(users, u);
-            }else if(j==2){
-                userModel.changeEmail(users, u);
-            }else if(j==3){
-                userModel.changePassword(u);
-            }else if(j==4){
+                }
+                else{
+                    int j = sc.nextInt();
+                if(j==1){
+                    userModel.changeUsername(users, u);
+                    }else if(j==2){
+                    userModel.changeEmail(users, u);
+                    }else if(j==3){
+                    userModel.changePassword(u);
+                    }else if(j==4){
 
-            }else if(j==0){
-                i=0;
-            }}
-        }}while(i != 0);
+                    }else if(j==0){
+                    i=0;
+                    }
+                }
+            }
+        }while(i != 0);
     }
-}
-     
+}  
