9a899c6155fcb52d242f39df5fd332ee2551b3b2
cheetah112 <nonpvp123@gmail.com>
2022-10-22 18:23:00 +0800

    Create Follower.java

---
+++ Java/Tiktok/Tiktok/src/Follower.java
@@ -0,0 +1,34 @@
+public class Follower {
+    private String name;
+    private int id;
+    private String mail;
+    private int numberOfLike;
+    
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    public int getNumberOfLike() {
+        return numberOfLike;
+    }
+    public void setNumberOfLike(int numberOfLike) {
+        this.numberOfLike = numberOfLike;
+    }
+
+    
+}
