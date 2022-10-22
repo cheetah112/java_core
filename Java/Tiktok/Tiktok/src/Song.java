50c20d93ec14f57a550c79e0e5b4586fdb9aab1a
cheetah112 <nonpvp123@gmail.com>
2022-10-22 18:23:03 +0800

    Create Song.java

---
+++ Java/Tiktok/Tiktok/src/Song.java
@@ -0,0 +1,26 @@
+public class Song {
+    private int id;
+    private String name;
+    private String singer;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSinger() {
+        return singer;
+    }
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    
+}   
