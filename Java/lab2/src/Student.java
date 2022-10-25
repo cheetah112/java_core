67941f88b0badffacc89c86ca963736d9eb3fae4
cheetah112 <nonpvp123@gmail.com>
2022-10-25 13:19:27 +0800

    Create Student.java

---
+++ Java/lab2/src/Student.java
@@ -0,0 +1,55 @@
+public class Student {
+private int id;
+private String name;
+private double scoreMath;
+private double scorePhysic;
+private double scoreChemistry;
+private double avgScore;
+private String rank;
+public Student() {
+}
+public int getId() {
+    return id;
+}
+public void setId(int id) {
+    this.id = id;
+}
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public double getScoreMath() {
+    return scoreMath;
+}
+public void setScoreMath(double scoreMath) {
+    this.scoreMath = scoreMath;
+}
+public double getScorePhysic() {
+    return scorePhysic;
+}
+public void setScorePhysic(double scorePhysic) {
+    this.scorePhysic = scorePhysic;
+}
+public double getScoreChemistry() {
+    return scoreChemistry;
+}
+public void setScoreChemistry(double scoreChemistry) {
+    this.scoreChemistry = scoreChemistry;
+}
+public double getAvgScore() {
+    return avgScore;
+}
+public void setAvgScore(double avgScore) {
+    this.avgScore = avgScore;
+}
+public String getRank() {
+    return rank;
+}
+public void setRank(String rank) {
+    this.rank = rank;
+}
+
+
+}
